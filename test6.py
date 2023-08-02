b64c13a6ab0a4f3864ff77a65aec6b072c3c83b1
ghada <ghadahamoudi737@gmail.com>
2023-08-02 12:36:29 +0100

    loops

---
+++ test6.py
@@ -0,0 +1,16 @@
+nums = [1, 2, 3, 4, 5]
+
+for i in nums:
+    if i == 3:
+        print('found')
+        break
+    print(i)
+
+for i in range(10):
+    print(i)
+
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
