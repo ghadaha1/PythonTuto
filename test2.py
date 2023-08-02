ad61e2157ca525a78fb6836cd2e628bf94c4aa86
ghada <ghadahamoudi737@gmail.com>
2023-08-02 09:40:52 +0100

    integers & float

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ num += 10
 print(num)
 
 print(abs(-3))
-print(round(7.35 ))
+print(round(7.35))
 
 num_1 = 3
 num_2 = 5
@@ -11,4 +11,10 @@ print(num_1 == num_2)
 print(num_1 != num_2)
 print(num_1 >= num_2)
 
+yo = '100'
+yo1 = '200'
+print(yo + yo1)  # treated like strings
 
+yoo = int(yo)
+yoo1 = int(yo1)
+print(yoo + yoo1) # casting string into integers
